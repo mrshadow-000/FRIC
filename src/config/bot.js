1a5d9a493ce24f0eb93a0cf8d80a576a76d80f68
mrshadow-000 <martonbene10@icloud.com>
2026-07-15 21:55:00 +0200

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -378,7 +378,7 @@ export const botConfig = {
     defaultGoodbyeMessage:
       "{user} has left the server. We now have {memberCount} members.",
     // Channel ID for welcome messages.
-    defaultWelcomeChannel: null,
+    defaultWelcomeChannel: 1527029523508494479,
     // Channel ID for goodbye messages.
     defaultGoodbyeChannel: null,
   },
